25e78fb500a09a5b85bcae192bf5ec2c924ec93e
“Rcardu” <djl1090523745@gmail.com>
2023-12-10 15:49:02 +0800

    配置系统

--- test_config/main.cc
+++ test_config/main.cc
@@ -1,12 +1,13 @@
 #include "config.h"
 
 
-Ricardo::ConfigVar<int>::ptr g_int_value_config = Ricardo::Config::Lookup("system.port",(int)8080,"system.port");
+Ricardo::ConfigVar<int>::ptr g_int_value_config = Ricardo::Config::Lookup("system.port",(int)8080,"system port");
+Ricardo::ConfigVar<float>::ptr g_float_value_config = Ricardo::Config::Lookup("system.value",(float)20.2f,"system value");
 
 int main (int argc, char* argv[]){
 
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT())<<g_int_value_config->getValue();
-    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())<<g_int_value_config->toString();
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT())<<g_float_value_config->toString();
 
 
 
