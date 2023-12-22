4081624bd765e22aa69554898b317ee2b1de47b2
“Rcardu” <djl1090523745@gmail.com>
2023-12-22 19:35:38 +0800

    测试文件可执行程序 编译添加IO协程调度模块 添加IO协程调度模块声明 添加IO协程调度模块实现 添加IO协程调度模块测试文件 源Yaml测试文件路径修改

---
+++ build/tests/CMakeFiles/appiomanager.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/Ricardo/Object/NetWork/Socket/SocketForCpp/bin/appiomanager"
+  "/home/Ricardo/Object/NetWork/Socket/SocketForCpp/bin/appiomanager.pdb"
+  "CMakeFiles/appiomanager.dir/iomanager_t.cc.o"
+  "CMakeFiles/appiomanager.dir/iomanager_t.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/appiomanager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/iomanager_t.cc
@@ -0,0 +1,47 @@
+#include "iomanager.h"
+#include "ricardo.h"
+
+Ricardo::Logger::ptr g_logger = ICEY_LOG_NAME("icey");
+
+void test_fiber() {
+  ICEY_LOG_INFO(g_logger) << "test_fiber";
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  fcntl(sock, F_SETFL, O_NONBLOCK);
+
+  sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(80);
+  inet_pton(AF_INET, "39.156.66.10", &addr.sin_addr.s_addr);
+
+  if (!connect(sock, (const sockaddr*)&addr, sizeof(addr))) {
+
+  } else if (errno == EINPROGRESS) {
+    ICEY_LOG_INFO(g_logger)
+        << "add event errno = " << errno << " " << strerror(errno);
+    Ricardo::IOManager::GetThis()->addEvent(
+        sock, Ricardo::IOManager::READ,
+        []() { ICEY_LOG_INFO(g_logger) << "read callback"; });
+
+    Ricardo::IOManager::GetThis()->addEvent(
+        sock, Ricardo::IOManager::WRITE, [&sock]() {
+          ICEY_LOG_INFO(g_logger) << "write callback";
+          //close(sock);
+          Ricardo::IOManager::GetThis()->cancelEvent(sock, Ricardo::IOManager::READ);
+          close(sock);
+        });
+  } else {
+    ICEY_LOG_INFO(g_logger) << "else " << errno << " " << strerror(errno);
+  }
+}
+
+void test1() {
+  std::cout << "EPOLLIN = " << EPOLLIN<< std::endl << "EPOLLOUT = " << EPOLLOUT << std::endl;
+  Ricardo::IOManager iom(2,false);
+  iom.schedule(&test_fiber);
+}
+
+int main(int argc, char* argv[]) {
+  test1();
+  return 0;
+}
